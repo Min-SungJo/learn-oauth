5f62b2bbf010ed1cad635945ab1714037689cc8e
민성조 <91239439+Min-SungJo@users.noreply.github.com>
2023-12-05 22:57:37 +0900

    Google login

---
+++ src/main/java/com/sj/learnoauth/OAuthSecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.sj.learnoauth;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class OAuthSecurityConfiguration {
+
+    @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+//        http.formLogin(withDefaults());
+//        http.httpBasic(withDefaults());
+        http.oauth2Login(Customizer.withDefaults());
+        return http.build();
+    }
+}
